19894ad82fe3d45d1cfe94c95ece7c3f665cbfff
emre <b101210026@sakarya.edu.tr>
2017-02-26 23:22:49 +0300

    prevent xss atack with validator

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express=require('express');
 var app=express();
 var server=require('http').createServer(app);
 var io=require('socket.io').listen(server);
+var validator=require('validator');
 
 users=[];
 connections=[];
@@ -19,16 +20,24 @@ io.sockets.on('connection',function(socket){
 
 
     socket.on('disconnect',function(data){
+        if(socket.userName){
+            users.splice(users.indexOf(socket));
+            io.sockets.emit('get users',{"userList":users});
+        }
+
         connections.splice(connections.indexOf(socket));
         console.log('Disconnected: %s sockets connected',connections.length);
     });
     socket.on('send message', function(data){
         console.log(data);
+        data=validator.escape(data);
+        data=socket.userName+" : "+data;
         io.sockets.emit('new message',{msg:data});
     });
 
     socket.on('new user', function(userName,callback){
         console.log("new user send name");
+        userName=validator.escape(userName);
         socket.userName=userName;
         users.push(userName);
         callback({"success":true,"userList":users});
